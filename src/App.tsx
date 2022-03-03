fa46405689b9399bd8489cc338e7051e5f4bea6f
Hemanth R <hemanthrt18@gmail.com>
2022-03-03 22:08:38 +0530

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { ThemeContext } from './context/ThemeContext';
 import './App.css';
 
+import { LandingPage } from './pages'
+import { Profile, Login, SignUp } from './components'
+import PrivateRoute from "./utils/PrivateRoute"
+import ScrollToTop from "./utils/ScrollToTop"
+
 function App() {
+
+  const { theme } = useContext(ThemeContext)
+
   return (
-    <div className="App">
-      <h1>HRT</h1>
+    <div className="app" data-theme={theme}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signin" element={<SignUp />} />
+            
+            <Route path="profile" 
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+
+
+            <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/auth/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Auth.css'
+
+
+function SignUp() {
+    return (
+      <div>SignUp</div>
+    )
+}
+
+export default SignUp
