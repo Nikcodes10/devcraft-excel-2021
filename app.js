c9bc2602397763b9d9f7970997f359711041903e
Nikhil George Mathew <gm.nikhilofficial@gmail.com>
2022-03-05 14:29:34 +0530

    cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,7 @@ mongoose
 .then(() => console.log("Connected to db"))
 .catch((err) => console.log("error in connecting to db" + err));
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
