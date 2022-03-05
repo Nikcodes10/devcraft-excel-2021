0e218a5ca3e6782a5a965ec47932abb95efe607a
Hemanth R <hemanthrt18@gmail.com>
2022-03-05 11:44:40 +0530

    profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Profile.css'
 
 function Profile() {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [country, setCountry] = useState('')
+
   return (
     <div className='profile'>
       <div className='profile__container'>
@@ -15,6 +21,36 @@ function Profile() {
           </div>
         </div>
         <h1>General</h1>
+        <div className='profile__general'>
+          <div className='profile_general_input'>
+            <label className='profile_label'>Name</label>
+            <input className='profile_input' type='text' value={name} onChange={(e) => setName(e.target.value)}/>
+          </div>
+          <div className='profile_general_input'>
+            <label className='profile_label'>Country</label>
+            <input className='profile_input' type='text' value={country} onChange={(e) => setCountry(e.target.value)}/>
+          </div>
+        </div>
+        <h1>Security</h1>
+        <div className='profile__general'>
+          <div className='profile_security_input'>
+            <label className='profile_label'>Email</label>
+            <div className='p_s_div'>
+              <input className='p_s__input' type='text' value={email} onChange={(e) => setEmail(e.target.value)}/>
+              <button className='profile__secondary_btn'>Change Email</button>
+            </div>
+          </div>
+          <div className='profile_security_input'>
+            <label className='profile_label'>Password</label>
+            <div className='p_s_div'>
+              <input className='p_s__input' type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <button className='profile__secondary_btn'>Change Password</button>
+            </div>
+          </div>
+        </div>
+        <div className='profile_save'>
+          <button className='profile__primary_btn'>Save</button>
+        </div>
       </div>
     </div>
   )
