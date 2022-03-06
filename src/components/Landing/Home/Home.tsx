833970d3fdd663c4b48ef0f0683914ae7a7a9c2e
Hemanth R <hemanthrt18@gmail.com>
2022-03-06 06:50:47 +0530

    landing

--- src/components/Landing/Home/Home.tsx
+++ src/components/Landing/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { Navbar } from '../..'
 
-
+import landing from '../../../assets/svg/landing.svg'
 import './Home.css'
 
 function Home() {
@@ -13,22 +13,19 @@ function Home() {
             <div className='home__container'>
                 <div className='home_left'>
                     <div className='home_left__content'>
-                        <h1>Smart Investments with HyFi bank</h1>
+                        <h1>The <span className='bold'>Smartest</span> Bank on your <span className='primary__color bold'>Smartphone</span></h1>
                         <p>
                             The easiste way to invest in mutual funds, obligation, money market and stock
                         </p>
                         <div className='home_btn_container'>
-                            <Link to='/home'>
-                                <button className='homeBtn home_g_btn'>Let's start</button>
+                            <Link to='/login'>
+                                <button className='home_g_btn'>Let's start</button>
                             </Link>
                         </div>
                     </div>
                 </div>
                 <div className='home_right'>
-                    <div className='home_right_content'>
-                        <h1>$590,568</h1>
-                        <h3>+80.05 / 0.5%</h3>
-                    </div>
+                    <img src={landing} alt="" className='landing_img'/>
                 </div>
             </div>
         </div>
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 import { Home } from '../../components'
 
+import './LandingPage.css'
+
 function LandingPage() {
     return (
-        <div>
+        <div className='landingPage'>
+            <div className="circle1"/>
+            <div className="circle2"/>
             <Home />
         </div>
     )
